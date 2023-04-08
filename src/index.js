d51466a9f0b96a7782b8ecdd2ecdccf212b86345
Leksa <admin@petiteweb.ru>
2023-04-08 13:03:39 +0300

    Начало работы с React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+  const A = Math.PI
+  export A
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  import A
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// Взять из html-файла тег, внутри которого будет работать React
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Собрать внутрь тега код из круглых скобок
+// root.render(
+//   React.createElement("h1", {title: "doggy"}, "Hello React!")
+// )
+
+// root.render(
+//   React.createElement(
+//     "div", 
+//     {title: "doggy"}, 
+//     React.createElement("h1", null, "Hello React"),
+//     React.createElement("p", {style:{color: "red"}}, "DogFood shop")
+//   )
+// )
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+    <h1>Hello React</h1>
+    <p>DogFood 
+      <br />
+    shop</p>
+    <i />
+  </div>
+)
